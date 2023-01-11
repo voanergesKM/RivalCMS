c4f220c122f1b5ba37738a87cda5a871d556f558
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-01-11 15:46:07 +0200

    try...catch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-import { MainLayout } from './components/MainLayout';
 import { HomePage } from './pages/HomePage/HomePage';
 import { LoginPage } from './pages/LoginPage/LoginPage';
 import { RegisterPage } from './pages/RegisterPage/RegisterPage';
@@ -7,11 +6,9 @@ import { RegisterPage } from './pages/RegisterPage/RegisterPage';
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<MainLayout />}> */}
-      <Route index element={<HomePage />} />
-      <Route path="login" element={<LoginPage />} />
-      <Route path="register" element={<RegisterPage />} />
-      {/* </Route> */}
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>
   );
 }
