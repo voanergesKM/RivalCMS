b2e411c3ff6efef0112ce3da79ba35a9987b33a8
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-01-11 15:08:59 +0200

    try...

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import { RegisterPage } from './pages/RegisterPage/RegisterPage';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
-      </Route>
+      {/* <Route path="/" element={<MainLayout />}> */}
+      <Route index element={<HomePage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<RegisterPage />} />
+      {/* </Route> */}
     </Routes>
   );
 }
