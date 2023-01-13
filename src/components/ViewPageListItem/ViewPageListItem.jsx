51c033ba1d41d1e04427465c452288563ad15865
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-01-13 17:48:53 +0200

    toggle page status

--- src/components/ViewPageListItem/ViewPageListItem.jsx
+++ src/components/ViewPageListItem/ViewPageListItem.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { MoreIcon } from '../../assets/icons/SvgIcons';
+import { changePageStatus } from '../../redux/addFilesSlice';
 import { msToTime } from '../../utils/convertMs';
 import styles from './ViewPageListItem.module.css';
 
 export const ViewPageListItem = ({
   item: { id, pageName, created, published, authorName, isAdmin },
 }) => {
+  const [status, setStatus] = useState(published);
+
   const currentDate = Date.now();
+  const dispatch = useDispatch();
+
+  const handleChangeStatus = id => {
+    setStatus(!status);
+    dispatch(changePageStatus(id));
+  };
 
   return (
     <li className={styles.pagesList__item}>
       <p className={styles.pagesList__item__title}>{pageName}</p>
       <p className={styles.pagesList__item__created}>{msToTime(created, currentDate)}</p>
       <p
+        onClick={() => handleChangeStatus(id)}
         className={
-          published ? styles.pagesList__item__badge : styles.pagesList__item__badgeUunpublished
+          status ? styles.pagesList__item__badge : styles.pagesList__item__badgeUunpublished
         }
       >
-        {published ? 'Published' : 'Unpublished'}
+        {status ? 'Published' : 'Unpublished'}
       </p>
       <div className={styles.pagesList__item__authorWrapper}>
         <p className={styles.pagesList__item__author}>{authorName}</p>
