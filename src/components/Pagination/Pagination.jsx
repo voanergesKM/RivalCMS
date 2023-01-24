35b84f804952c101fe3671f8fa061dbbe9525020
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-01-24 16:35:40 +0200

    pagination...

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,64 +1,66 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import usePagination from '../../hooks/usePagination';
 import { FileItem } from '../FileItem/FileItem';
 
 import styles from './Pagination.module.css';
 
-//add input for specific page
-
 function Pagination({ pages }) {
-  const [items, setItems] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams({ page: 1 });
 
   useEffect(() => {
-    setItems(pages);
-  }, [pages]);
+    const page = searchParams?.get('page');
 
-  const {
-    firstContentIndex,
-    lastContentIndex,
-    nextPage,
-    prevPage,
-    page,
-    gaps,
-    setPage,
-    totalPages,
-  } = usePagination({
-    contentPerPage: 5,
-    count: items.length,
-  });
-  console.log('Pagination : totalPages', totalPages);
+    setSearchParams({ page: '1' } && { page: page });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // useEffect(() => {
+  //   const currentPage = Number(searchParams.get('page'));
+  //   const desiredPages = Math.ceil(pages.length / 5);
+
+  //   if (desiredPages < currentPage) {
+  //     console.log('useEffect : currentPage', currentPage);
+  //     console.log('useEffect : desiredPages', desiredPages);
+  //     setSearchParams({ page: desiredPages });
+  //   }
+  // }, [pages]);
+
+  const { firstContentIndex, lastContentIndex, nextPage, prevPage, page, gaps, totalPages } =
+    usePagination({
+      contentPerPage: 5,
+      count: pages.length,
+    });
 
   return (
     <>
       <ul style={{ flexGrow: 1 }}>
-        {items.slice(firstContentIndex, lastContentIndex).map(file => (
-          <FileItem key={file.id} file={file} />
-        ))}
+        {pages &&
+          pages
+            .slice(firstContentIndex, lastContentIndex)
+            .map(file => <FileItem key={file.id} file={file} />)}
       </ul>
 
       {totalPages > 0 && (
         <div className={styles.paginationContainer}>
-          <button
-            onClick={prevPage}
-            className={page === 1 ? 'hidden' : styles.paginationItem}
-            // className={styles.paginationItem}
-          >
+          <button onClick={prevPage} className={page === 1 ? 'hidden' : styles.paginationItem}>
             &larr;
           </button>
           <button
-            onClick={() => setPage(1)}
-            // className={`page ${page === 1 && 'disabled'}`}
+            onClick={() => {
+              setSearchParams({ page: 1 });
+            }}
             className={page === 1 ? styles.paginationItemActive : styles.paginationItem}
           >
             1
           </button>
           {gaps.before ? '...' : null}
-          {/* @ts-ignore */}
           {gaps.paginationGroup.map(el => (
             <button
-              onClick={() => setPage(el)}
+              onClick={() => {
+                setSearchParams({ page: el });
+              }}
               key={el}
-              // className={`page ${page === el ? 'active' : ''}`}
               className={page === el ? styles.paginationItemActive : styles.paginationItem}
             >
               {el}
@@ -66,7 +68,9 @@ function Pagination({ pages }) {
           ))}
           {gaps.after ? '...' : null}
           <button
-            onClick={() => setPage(totalPages)}
+            onClick={() => {
+              setSearchParams({ page: totalPages });
+            }}
             className={page === totalPages ? styles.paginationItemActive : styles.paginationItem}
           >
             {totalPages}
