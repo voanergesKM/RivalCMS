dbe603c044c0dfa9116cf14b610ab38269c218be
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-01-28 15:10:10 +0200

    userLayout response

--- src/assets/icons/SvgIcons.jsx
+++ src/assets/icons/SvgIcons.jsx
@@ -690,3 +690,37 @@ export const DescendingIcon = ({ size }) => {
     </svg>
   );
 };
+
+export const OpenMenuIcon = ({ size }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g stroke="#63B3ED" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5">
+        <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10ZM8.5 12h6" />
+        <path d="m12.5 15 3-3-3-3" />
+      </g>
+    </svg>
+  );
+};
+
+export const CloseMenuIcon = ({ size }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g stroke="#63B3ED" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5">
+        <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10ZM15.5 12h-6" />
+        <path d="m11.5 9-3 3 3 3" />
+      </g>
+    </svg>
+  );
+};
