872edea9dc2ccf96703161246048320662d556eb
Oleksandr Kurzhy <103747805+voanergesKM@users.noreply.github.com>
2023-01-30 17:32:31 +0200

    create page resp added

--- src/pages/CreatePage/CreatePage.jsx
+++ src/pages/CreatePage/CreatePage.jsx
@@ -16,7 +16,6 @@ export const CreatePage = () => {
 
   const user = useSelector(state => state.loggedUser);
   const pages = useSelector(state => state.sitePages);
-  console.log('CreatePage : pages', pages);
 
   const dispatch = useDispatch();
 
@@ -141,7 +140,7 @@ export const CreatePage = () => {
           {formik.errors.pageName && (
             <ValidationErrorMessage>{formik.errors.pageName}</ValidationErrorMessage>
           )}
-          <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div className={styles.userWrapper}>
             <User authorName={user.name} isAdmin={user.isAdmin} style={{ marginRight: '2.5rem' }} />
             <button
               type="button"
@@ -177,152 +176,3 @@ export const CreatePage = () => {
     </UserLayout>
   );
 };
-
-// export const CreatePage = () => {
-//   const [selectedFile, setSelectedFile] = useState(null);
-//   const [pageName, setPageName] = useState('');
-//   const [isBtnDisabled, setIsBtnDisabled] = useState(true);
-//   const [isPageNameInputDisabled, setIsPageNameInputDisabled] = useState(true);
-
-//   const user = useSelector(state => state.loggedUser);
-
-//   const dispatch = useDispatch();
-
-//   const fileInput = useRef();
-
-//   const handleSubmit = evt => {
-//     evt.preventDefault();
-
-//     if (!selectedFile) {
-//       return alert('Please, select an html file');
-//     }
-
-//     const pageName = evt.target[0].value;
-
-//     if (!pageName) {
-//       return;
-//     }
-
-//     const { lastModified } = selectedFile;
-//     const { name, isAdmin } = user;
-
-//     dispatch(addSitePage({ pageName, lastModified, name, isAdmin }));
-
-//     setSelectedFile(null);
-//     setPageName('');
-//     setIsBtnDisabled(true);
-//     alert(`File ${pageName} was succesfully added`);
-//   };
-
-//   const handleChangePageName = evt => {
-//     if (typeof evt === 'object') {
-//       setPageName(evt.target.value);
-//       return;
-//     }
-//     setPageName(evt);
-//   };
-
-//   const handleFileChange = evt => {
-//     evt.preventDefault();
-
-//     setSelectedFile(evt.target.files[0]);
-
-//     const { name } = evt.target.files[0];
-//     const fileName = name.slice(0, name.indexOf('.'));
-//     handleChangePageName(fileName);
-//     setIsBtnDisabled(false);
-//   };
-
-//   const handleDrop = evt => {
-//     evt.preventDefault();
-
-//     const { name } = evt.dataTransfer.files[0];
-
-//     const restricdedFile = name.slice(name.indexOf('.'));
-
-//     if (restricdedFile !== '.html') {
-//       console.log(selectedFile);
-//       return alert('Please, select an html file');
-//     }
-
-//     const fileName = name.slice(0, name.indexOf('.'));
-
-//     setSelectedFile(evt.dataTransfer.files[0]);
-//     handleChangePageName(fileName);
-//     setIsBtnDisabled(false);
-//   };
-
-//   const handleDrag = evt => {
-//     evt.preventDefault();
-//   };
-
-//   const toggleEditFileName = () => {
-//     if (!selectedFile) {
-//       return alert('Please, select file');
-//     }
-//     setIsPageNameInputDisabled(prevState => !prevState);
-//   };
-
-//   return (
-//     <UserLayout>
-//       <div className={styles.createPage}>
-//         <div className={styles.createPage__header}>
-//           <form className={styles.createPage__form} onSubmit={handleSubmit}>
-//             <label
-//               onClick={toggleEditFileName}
-//               className={styles.createPage__form__label}
-//               htmlFor="pageName"
-//             >
-//               <EditIcon size={24} />
-//             </label>
-//             <input
-//               className={styles.createPage__form__input}
-//               id="pageName"
-//               name="pageName"
-//               type="text"
-//               onChange={handleChangePageName}
-//               value={pageName}
-//               minLength={3}
-//               maxLength={20}
-//               disabled={isPageNameInputDisabled}
-//             />
-//             <div style={{ display: 'flex', alignItems: 'center' }}>
-//               <User
-//                 authorName={user.name}
-//                 isAdmin={user.isAdmin}
-//                 style={{ marginRight: '2.5rem' }}
-//               />
-//               <button
-//                 type="submit"
-//                 className={styles.createPage__form__btn}
-//                 disabled={isBtnDisabled}
-//               >
-//                 <EyeIcon size={24} />
-//                 Add page
-//               </button>
-//             </div>
-//           </form>
-//         </div>
-//         <div
-//           onClick={() => fileInput.current.click()}
-//           className={styles.upload}
-//           onDragStart={handleDrag}
-//           onDragLeave={handleDrag}
-//           onDragOver={handleDrag}
-//           onDrop={handleDrop}
-//         >
-//           <button type="button" className={styles.upload__actions} disabled>
-//             <PlusIcon size={24} /> Add new section
-//           </button>
-//           <input
-//             accept=".html"
-//             ref={fileInput}
-//             className="hidden"
-//             onChange={handleFileChange}
-//             type="file"
-//           />
-//         </div>
-//       </div>
-//     </UserLayout>
-//   );
-// };
